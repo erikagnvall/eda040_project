049ec2ad1db01af4c35c8db98e4902370f418b76
Erik Jansson <erikjansson90@gmail.com>
2011-11-03 15:15:18 +0100

    impl awaitImage

--- Client/src/se/lth/student/eda040/a1/network/ClientProtocol.java
+++ Client/src/se/lth/student/eda040/a1/network/ClientProtocol.java
@@ -1,13 +1,37 @@
 package se.lth.student.eda040.a1.network;
 
-public class ClientProtocol {
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
 
-	public ClientProtocol() {
-		
+public class ClientProtocol {
+	private int cameraId;
+	private Socket socket;
+
+	public ClientProtocol(Socket socket, int cameraId) {
+		this.cameraId = cameraId;
+		this.socket = socket;
 	}
 
-	public Image awaitImage(){
-		return null;
+	public Image awaitImage() throws IOException{
+		InputStream is = socket.getInputStream();
+		byte[] buffer = new byte[5];
+		int bytesRead = 0;
+		while (bytesRead < 5) {
+			is.read(buffer, bytesRead, 5-bytesRead);
+		}
+		int imageLen = 0;
+		imageLen |= buffer[1] << 24;
+		imageLen |= buffer[2] << 16;
+		imageLen |= buffer[3] << 8;
+		imageLen |= buffer[4];
+		bytesRead = 0;
+		byte[] imageData = new byte[imageLen];
+		while(bytesRead < imageLen){
+			is.read(imageData, bytesRead, imageLen-bytesRead);
+		}
+		Image img = new Image(imageData, ((int) buffer[0]) == 1); // TODO fix constant value
+		return img;
 	}
 
 	public void sendCommand(Command cmd){
@@ -15,13 +39,11 @@ public class ClientProtocol {
 	}
 
 	public int getCameraId() {
-		// TODO Auto-generated method stub
-		return 0;
+		return cameraId;
 	}
-
-	public int getID() {
-		// TODO Auto-generated method stub
-		return 0;
+	
+	public void disconnect() throws IOException {
+		socket.close();
 	}
 
 }
