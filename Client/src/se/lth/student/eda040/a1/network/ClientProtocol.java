16efa30e6da865be4c997e62b6f0fa225690b18d
Oscar Olsson <osse.olsson@gmail.com>
2011-11-03 15:46:32 +0100

    impl ClientProtocol and Command

--- Client/src/se/lth/student/eda040/a1/network/ClientProtocol.java
+++ Client/src/se/lth/student/eda040/a1/network/ClientProtocol.java
@@ -2,6 +2,7 @@ package se.lth.student.eda040.a1.network;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class ClientProtocol {
@@ -18,7 +19,7 @@ public class ClientProtocol {
 		byte[] buffer = new byte[5];
 		int bytesRead = 0;
 		while (bytesRead < 5) {
-			is.read(buffer, bytesRead, 5-bytesRead);
+			bytesRead += is.read(buffer, bytesRead, 5-bytesRead);
 		}
 		int imageLen = 0;
 		imageLen |= buffer[1] << 24;
@@ -28,13 +29,15 @@ public class ClientProtocol {
 		bytesRead = 0;
 		byte[] imageData = new byte[imageLen];
 		while(bytesRead < imageLen){
-			is.read(imageData, bytesRead, imageLen-bytesRead);
+			bytesRead += is.read(imageData, bytesRead, imageLen-bytesRead);
 		}
 		Image img = new Image(imageData, ((int) buffer[0]) == 1); // TODO fix constant value
 		return img;
 	}
 
-	public void sendCommand(Command cmd){
+	public void sendCommand(Command cmd) throws IOException{
+		OutputStream os = socket.getOutputStream();
+		os.write(cmd.getCommand());
 		return;
 	}
 
--- Client/src/se/lth/student/eda040/a1/network/Command.java
+++ Client/src/se/lth/student/eda040/a1/network/Command.java
@@ -1,11 +1,17 @@
 package se.lth.student.eda040.a1.network;
 
 public class Command {
-	// constants...
-	private int cameraID;
-	
-	public Command() {
-		
+	public static byte MODE_IDLE = 0;
+	public static byte MODE_VIDEO = 1;
+	public static byte DISCONNECT = 2;
+
+	private byte command;
+	public Command(byte command) {
+		this.command = command;
+	}
+
+	public byte getCommand() {
+		return command;
 	}
 }
 
