c9b7d3e93f7586b7be22f73d23944595cbb34807
Oscar Olsson <osse.olsson@gmail.com>
2011-11-04 02:25:26 +0100

    added junit tests to build.xml

---
+++ Client/src/tests/TrivialTest.java
@@ -0,0 +1,15 @@
+package tests;
+
+import junit.framework.TestCase;
+
+public class TrivialTest extends TestCase {
+
+    public TrivialTest(){
+
+    }
+
+    public void testTrivial(){
+        System.out.println("HERE");
+        assertTrue(true);
+    }
+}
