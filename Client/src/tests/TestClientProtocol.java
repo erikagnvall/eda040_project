f36ca26e31bc8b3d19961645889406c3d489ce9b
Oscar Olsson <osse.olsson@gmail.com>
2011-11-07 17:50:27 +0100

    Added additional tests for ClientProtocol

--- Client/src/tests/TestClientProtocol.java
+++ Client/src/tests/TestClientProtocol.java
@@ -15,7 +15,7 @@ public class TestClientProtocol extends TestCase {
 	public TestClientProtocol(){
 	}
 
-	public void testGetSimpleImaga(){
+	public void testGetSimpleImage(){
 		byte[] input = {0x01, 0x00, 0x00, 0x00, 0x01, 0x0F};
 		byte[] imageData = {0x0F};
 		ClientProtocol cp = new ClientProtocol(new MockSocket(input), 0);
@@ -23,7 +23,38 @@ public class TestClientProtocol extends TestCase {
 			Image img = cp.awaitImage();
 			assertTrue("Excpetced: " + imageData + " Got: " + img.getImageData(), Arrays.equals(imageData, img.getImageData()));
 			assertEquals("Incorrect camera ID", 0, img.getCameraId());
-			assertTrue("Incorrect mode", img.isVideoMode());
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("Got Exception but shouldnt have");
+		}
+	}
+
+	public void testImageTooLong(){
+		byte[] input = {0x01, 0x00, 0x00, 0x00, 0x01, 0x0F, 0x01, 0x01, 0x01};
+		byte[] imageData = {0x0F};
+		ClientProtocol cp = new ClientProtocol(new MockSocket(input), 0);
+		try {
+			Image img = cp.awaitImage();
+			assertTrue("Excpetced: " + imageData + " Got: " + img.getImageData(), Arrays.equals(imageData, img.getImageData()));
+			assertEquals("Incorrect camera ID", 0, img.getCameraId());
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("Got Exception but shouldnt have");
+		}
+	}
+
+	public void testDoubleImage(){
+		byte[] input = {0x01, 0x00, 0x00, 0x00, 0x01, 0x0F, 0x01, 0x00, 0x00, 0x00, 0x02, 0x0F, 0x0F};
+		byte[] imageData_1 = {0x0F};
+		byte[] imageData_2 = {0x0F, 0x0F};
+		ClientProtocol cp = new ClientProtocol(new MockSocket(input), 0);
+		try {
+			Image img = cp.awaitImage();
+			assertTrue("Excpetced: " + imageData_1 + " Got: " + img.getImageData(), Arrays.equals(imageData_1, img.getImageData()));
+			assertEquals("Incorrect camera ID", 0, img.getCameraId());
+			img = cp.awaitImage();
+			assertTrue("Excpetced: " + imageData_2 + " Got: " + img.getImageData(), Arrays.equals(imageData_2, img.getImageData()));
+			assertEquals("Incorrect camera ID", 0, img.getCameraId());
 		} catch (IOException e) {
 			e.printStackTrace();
 			fail("Got Exception but shouldnt have");
