883acf605f8bbfd829f53fa75694fb120c24d1d4
Oscar Olsson <osse.olsson@gmail.com>
2011-11-17 17:34:14 +0100

    Added some test cases of ClientProtocol

--- Client/tests/tests/TestClientProtocol.java
+++ Client/tests/tests/TestClientProtocol.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
 import com.xtremelabs.robolectric.RobolectricTestRunner;
 
@@ -24,19 +25,34 @@ public class TestClientProtocol {
 	public TestClientProtocol(){
 	}
 
-	@Test
-	public void testGetSimpleImage(){
-		byte[] input = {0x01, 0x00, 0x00, 0x00, 0x20, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F};
-		byte[] imageData = {0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F, 0x0F};
-		ClientProtocol cp = new ClientProtocol((byte)0);
+	private void connectAndSendInput(ClientProtocol cp, byte[] input) throws IOException, InterruptedException{
 		ServerState state = new ServerState();
 		MockServer ms = new MockServer(input, state);
 		ms.start();
+		state.waitConnected();
+		cp.connectTo("127.0.0.1");
+	}
+
+	@Test
+	public void testGetSimpleImage(){
+		byte[] input = new byte[37];
+		byte[] expectedImageData = new byte[32];
+		input[0] = 0;
+		input[1] = 0;
+		input[2] = 0;
+		input[3] = 0;
+		input[4] = (byte)32;
+		for(int i = 0; i < input[4]; i++){
+			input[i + 5] = (byte)200;
+			expectedImageData[i] = (byte)200;
+		}
+
+		ClientProtocol cp = new ClientProtocol((byte)0);
 		try {
-			state.waitConnected();
-			cp.connectTo("127.0.0.1");
+			connectAndSendInput(cp, input);
 			Image img = cp.awaitImage();
-			assertTrue("Excpetced: " + imageData + " Got: " + img.getImageData(), Arrays.equals(imageData, img.getImageData()));
+			assertTrue("Excpetced: " + expectedImageData + " Got: " + img.getImageData(),
+					Arrays.equals(expectedImageData, img.getImageData()));
 			assertEquals("Incorrect camera ID", 0, img.getCameraId());
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -45,6 +61,144 @@ public class TestClientProtocol {
 			//
 		}
 		
+	}
+
+	@Test
+	public void testImageTooLong(){
+		byte[] input = new byte[39];
+		byte[] expectedImageData = new byte[32];
+		input[0] = 0;
+		input[1] = 0;
+		input[2] = 0;
+		input[3] = 0;
+		input[4] = (byte)32;
+		for(int i = 0; i < input[4]; i++){
+			input[i + 5] = (byte)200;
+			expectedImageData[i] = (byte)200;
+		}
+
+		ClientProtocol cp = new ClientProtocol((byte)0);
+		try {
+			connectAndSendInput(cp, input);
+			Image img = cp.awaitImage();
+			assertTrue("Excpetced: " + expectedImageData + " Got: " + img.getImageData(),
+					Arrays.equals(expectedImageData, img.getImageData()));
+			assertEquals("Incorrect camera ID", 0, img.getCameraId());
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("Got Exception but shouldnt have");
+		} catch (InterruptedException e){
+			//
+		}
+
+	}
+
+	@Test
+	public void testDoubleImage(){
+		byte[] input = new byte[74];
+		byte[] expectedImageData_1 = new byte[32];
+		byte[] expectedImageData_2 = new byte[32];
+		input[0] = 0;
+		input[1] = 0;
+		input[2] = 0;
+		input[3] = 0;
+		input[4] = (byte)32;
+		input[37] = 0;
+		input[38] = 0;
+		input[39] = 0;
+		input[40] = 0;
+		input[41] = (byte)32;
+		for(int i = 0; i < input[4]; i++){
+			input[i + 5] = (byte)200;
+			expectedImageData_1[i] = (byte)200;
+		}
+		for(int i = 0; i < input[41]; i++){
+			input[i + 42] = (byte)200;
+			expectedImageData_2[i] = (byte)200;
+		}
+		ClientProtocol cp = new ClientProtocol((byte)0);
+		try {
+			connectAndSendInput(cp, input);
+			Image img = cp.awaitImage();
+			assertTrue("Excpetced: " + expectedImageData_1 + " Got: " + img.getImageData(),
+					Arrays.equals(expectedImageData_1, img.getImageData()));
+			assertEquals("Incorrect camera ID", 0, img.getCameraId());
+			img = cp.awaitImage();
+			assertTrue("Excpetced: " + expectedImageData_2 + " Got: " + img.getImageData(),
+					Arrays.equals(expectedImageData_2, img.getImageData()));
+			assertEquals("Incorrect camera ID", 0, img.getCameraId());
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("Got Exception but shouldnt have");
+		} catch (InterruptedException e){
+			//
+		}
+
+	}
+
+	@Test
+	public void testVideoMode(){
+		byte[] input = new byte[37];
+		input[0] = 'v';
+		input[1] = 0;
+		input[2] = 0;
+		input[3] = 0;
+		input[4] = (byte)32;
+		for(int i = 0; i < input[4]; i++){
+			input[i + 5] = (byte)200;
+		}
+		ClientProtocol cp = new ClientProtocol((byte)0);
+		try {
+			connectAndSendInput(cp, input);
+			Image img = cp.awaitImage();
+			assertTrue("Expected vieo mode, but wasn't", img.isVideoMode()); 
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("Got Exception but shouldnt have");
+		} catch (InterruptedException e){
+			//
+		}
+
+	}
+
+	@Test
+	public void testNotVideoMode(){
+		byte[] input = new byte[37];
+		input[0] = 'i';
+		input[1] = 0;
+		input[2] = 0;
+		input[3] = 0;
+		input[4] = (byte)32;
+		for(int i = 0; i < input[4]; i++){
+			input[i + 5] = (byte)200;
+		}
+		ClientProtocol cp = new ClientProtocol((byte)0);
+		try {
+			connectAndSendInput(cp, input);
+			Image img = cp.awaitImage();
+			assertFalse("Expected not video mode, but was.", img.isVideoMode()); 
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("Got Exception but shouldnt have");
+		} catch (InterruptedException e){
+			//
+		}
+	}
+
+	@Test
+	public void testLostConnection(){
+		byte[] input = {'i', 0x00, 0x00, 0x00, 0x02, 0x0F};
+		ClientProtocol cp = new ClientProtocol((byte)0);
+		try {
+			connectAndSendInput(cp, input);
+			Image img = cp.awaitImage();
+			fail("Should have throw an IOException but didn't");
+		} catch (IOException e) {
+			assertEquals("Connection lost", e.getMessage());
+		} catch (InterruptedException e){
+			//
+		}
+
 	}
 
 	private class ServerState {
@@ -91,92 +245,5 @@ public class TestClientProtocol {
 			}
 		}
 	}
-
-	/**
-	public void testImageTooLong(){
-		byte[] input = {0x01, 0x00, 0x00, 0x00, 0x01, 0x0F, 0x01, 0x01, 0x01};
-		byte[] imageData = {0x0F};
-		ClientProtocol cp = new ClientProtocol(new MockSocket(input), 0);
-		try {
-			Image img = cp.awaitImage();
-			assertTrue("Excpetced: " + imageData + " Got: " + img.getImageData(), Arrays.equals(imageData, img.getImageData()));
-			assertEquals("Incorrect camera ID", 0, img.getCameraId());
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail("Got Exception but shouldnt have");
-		}
-	}
-
-	public void testDoubleImage(){
-		byte[] input = {0x01, 0x00, 0x00, 0x00, 0x01, 0x0F, 0x01, 0x00, 0x00, 0x00, 0x02, 0x0F, 0x0F};
-		byte[] imageData_1 = {0x0F};
-		byte[] imageData_2 = {0x0F, 0x0F};
-		ClientProtocol cp = new ClientProtocol(new MockSocket(input), 0);
-		try {
-			Image img = cp.awaitImage();
-			assertTrue("Excpetced: " + imageData_1 + " Got: " + img.getImageData(), Arrays.equals(imageData_1, img.getImageData()));
-			assertEquals("Incorrect camera ID", 0, img.getCameraId());
-			img = cp.awaitImage();
-			assertTrue("Excpetced: " + imageData_2 + " Got: " + img.getImageData(), Arrays.equals(imageData_2, img.getImageData()));
-			assertEquals("Incorrect camera ID", 0, img.getCameraId());
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail("Got Exception but shouldnt have");
-		}
-	}
-
-	public void testVideoMode(){
-		byte[] input = {'v', 0x00, 0x00, 0x00, 0x01, 0x0F};
-		ClientProtocol cp = new ClientProtocol(new MockSocket(input), 0);
-		try {
-			Image img = cp.awaitImage();
-			assertTrue("Expected vieo mode, but wasn't", img.isVideoMode()); 
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail("Got Exception but shouldnt have");
-		}
-	}
-
-	public void testNotVideoMode(){
-		byte[] input = {'i', 0x00, 0x00, 0x00, 0x01, 0x0F};
-		ClientProtocol cp = new ClientProtocol(new MockSocket(input), 0);
-		try {
-			Image img = cp.awaitImage();
-			assertFalse("Expected vieo mode, but wasn't", img.isVideoMode()); 
-		} catch (IOException e) {
-			e.printStackTrace();
-			fail("Got Exception but shouldnt have");
-		}
-	}
-
-	public void testLostConnection(){
-		byte[] input = {'i', 0x00, 0x00, 0x00, 0x02, 0x0F};
-		ClientProtocol cp = new ClientProtocol(new MockSocket(input), 0);
-		try {
-			Image img = cp.awaitImage();
-			fail("Should have throw an IOException but didn't");
-		} catch (IOException e) {
-			assertEquals("Connection lost", e.getMessage());
-		}
-	}
-
-	private class MockSocket extends Socket {
-		private java.io.InputStream in;
-		private java.io.OutputStream out;
-
-		public MockSocket(byte[] in){
-			this.in = new ByteArrayInputStream(in);
-			this.out = new ByteArrayOutputStream();
-		}
-
-		public InputStream getInputStream(){
-			return in;
-		}
-
-		public OutputStream getOutputStream(){
-			return out;
-		}
-	}
-	*/
 
 }
